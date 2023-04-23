bd65f6d49f431fb771e4ad6e2495d07bc9d1092a
dikshasolanki22 <dikshasolanki22@gmail.com>
2023-04-23 19:54:49 +0530

    Arrays 1D Basic Practice

--- Arrays/arrayBasic.java
+++ Arrays/arrayBasic.java
@@ -16,17 +16,23 @@ public class arrayBasic{
         int n=scn.nextInt();
 
         int[] array= new int[n];
+        System.out.println("Enter the elements:");
         inputArr(array);
 
-        int[] ans= minAndMax(array);
+      //  int[] ans= minAndMax(array);
 
-         System.out.println("Min ele: "+ans[0]);
-         System.out.println("Max ele: "+ans[1]);
-         System.out.println("Span of Array: "+spanOfArray(array));
+       //  System.out.println("Min ele: "+ans[0]);
+       //  System.out.println("Max ele: "+ans[1]);
+       //  System.out.println("Span of Array: "+spanOfArray(array));
 
-         System.out.println("Enter ele to be searched");
-         int ele= scn.nextInt();
-         System.out.println("Ele was found: "+findEle(array,ele));
+       //  System.out.println("Enter ele to be searched");
+      //   int ele= scn.nextInt();
+      //   System.out.println("Ele was found: "+findEle(array,ele));
+
+         //   reverseArrayBruteForce(array);
+          //  reverseArray(array);
+          //  printArr(array);
+          barChart(array);
          
     }
 
@@ -104,4 +110,124 @@ public class arrayBasic{
         return idx;
     }
 
+    public static void reverseArrayBruteForce(int[] arr)
+    {
+        // This takes O(n) extra space
+        int [] arrCopy= new int[arr.length];
+
+        for(int i=0; i<arr.length; i++)
+        {
+            arrCopy[arr.length-1-i]= arr[i];
+        }
+
+        for(int i=0; i<arr.length; i++)
+        {
+            arr[i]= arrCopy[i];
+        }
+    }
+
+    public static void reverseArray(int[] arr)
+    {
+        //By swapping till middle of the array by using two pointers
+        int left=0;
+        int right= arr.length-1;
+
+        while(left<right)
+        {
+        int temp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+
+        left++;
+        right--;
+        }
+    }
+
+    // Rotate an array by k 
+    // positive k= move elements from right to left(end to front)
+    // negative k= move elements from left to right(front to end)
+    public static void kRotate(int k,int []arr)
+    {
+        k=k%arr.length; // bring k in range
+
+        if(k<0) // For negative k, change it to positive
+            k=k+arr.length;
+
+        
+        int left=0;
+        int right= arr.length-1;
+        
+        reverseArrKRotate(arr,left,right);
+        reverseArrKRotate(arr,left,k-1);
+        reverseArrKRotate(arr,k,right);
+
+    }
+
+    public static void reverseArrKRotate(int[] arr,int start, int end)
+    {
+        while(start<end)
+        {
+            int temp= arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    public void rotateBruteForce(int[] nums, int k) {
+        
+        k=k%nums.length;
+        if(k<0)
+          k=k+nums.length;
+          
+        for(int j=0; j<k; j++)
+        {
+            int temp= nums[nums.length-1];
+
+            for(int i=nums.length-1; i>0; i--)
+            {
+                nums[i]=nums[i-1];
+            }
+            nums[0]=temp;
+        }
+    }
+
+    public void rotateLoveBabbar(int[] nums, int k) {
+        
+        int temp[]= new int[nums.length];
+
+        for(int i=0; i<nums.length; i++)
+        {
+            temp[(i+k)%nums.length]=nums[i];
+        }
+
+        for(int i=0; i<nums.length; i++)
+        {
+            nums[i]=temp[i];
+        }
+
+    }
+
+    public static void barChart(int[] arr)
+    {
+
+        int[] maxmin= minAndMax(arr);
+        int max= maxmin[1];
+
+        for(int y=max; y>0; y--)
+        {
+             for(int x=0;x<arr.length; x++)
+            {
+                int val=arr[x];
+                if(y<=val)
+                   System.out.print("*");
+                else
+                   System.out.print("-");
+            }
+            System.out.println();
+        }
+    }
 }
+
